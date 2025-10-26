d0c3a35ecd7f28951dddd16165070fb4a17ae22e
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-10-25 23:25:07 -0300

    feat: adiciona backend da aplicação

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from deep_translator import GoogleTranslator
+import logging
+
+app = Flask(__name__)
+CORS(app)
+
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route('/traduzir', methods=['POST'])
+def traduzir_texto():
+
+    try:
+        dados = request.get_json()
+        if not dados or 'texto' not in dados or 'destino' not in dados:
+            return jsonify({"erro": "Dados incompletos. 'texto' e 'destino' são obrigatórios."}), 400
+
+        texto_original = dados.get('texto')
+        idioma_destino = dados.get('destino')
+
+        if not texto_original.strip():
+            return jsonify({"texto_traduzido": ""}), 200
+
+        texto_traduzido = GoogleTranslator(source='auto', target=idioma_destino).translate(texto_original)
+        return jsonify({
+            "texto_traduzido": texto_traduzido
+        }), 200
+
+    except Exception as e:
+        app.logger.error(f"Erro na tradução: {e}")
+        return jsonify({"erro": f"Ocorreu um erro no servidor: {e}"}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
